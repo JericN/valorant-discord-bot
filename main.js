1493d116d9f1889b60b98670092cd56a4da877c5
Jeric <jericnarte912@gmail.com>
2021-08-06 03:45:41 +0800

    v0.31
    update token

--- main.js
+++ main.js
@@ -4,13 +4,11 @@ const client = new Discord.Client({partials:["MESSAGE", "CHANNEL", "REACTION"]})
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 
-from ping import ping
-
 ['command_handler', 'event_handler'].forEach(handler => {
     require('./handlers/'+handler)(client, Discord);
 })
 
-client.login('ODU3MzgxNzA5MzcyMTk0ODM2.YNOw8A.8SRx3ZZN5TwT5nhf4mC_jFhzLaU');
+client.login(process.env.TOKEN);
 
 //val id#tag stat s/summary
 //val id#tag stat a/gent
